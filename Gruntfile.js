65a27910d874b8744796676c7e5d0fcf8102c9bb
luetkemj <luetkemj@gmail.com>
2014-09-24 15:14:18 -0400

    fix base directory on grunt launch

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
             options: {
                 watchTask: true, // < VERY important
                 server: {
-                    baseDir: "./"
+                    baseDir: "./app/"
                 }
             }
         },
